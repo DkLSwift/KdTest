5ebd67bee516190892ccf7b6f2ef4e2314256a1e
Requena Eric <e.requena.dev@gmail.com>
2019-07-23 11:30:03 +0200

    Initial Commit

---
+++ Kd_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kd_test
+//
+//  Created by Eric DkL on 23/07/2019.
+//  Copyright © 2019 Eric DkL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
